6a15c328b22cf9dae3218fef413c4d0f5733aacf
Ashley-Wilson1 <ashley03wilson@gmail.com>
2024-11-23 23:35:26 +0000

    Changes to naming for login
    login name was causing errors so added an extra n

--- todo/todo/views.py
+++ todo/todo/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from todo import models
 from todo.models import TODO
+from django.contrib.auth import authenticate,login,logout
 
 
 def signup(request):
@@ -12,9 +13,20 @@ def signup(request):
         print(fnm,emailid,pwd)
         my_user = User.objects.create_user(fnm,emailid,pwd)
         my_user.save()
-        return redirect('/login')
+        return redirect('/loginn')
 
     return render(request, 'signup.html')
 
-def login(request):
-    return render(request,'login.html')
+def loginn(request):
+    if request.method == 'POST':
+        fnm = request.POST.get('fnm')
+        pwd = request.POST.get('pwd')
+        print(fnm, pwd)
+        user = authenticate(request, username =fnm, password = pwd)
+        if user is not None:
+            login(request,user)
+            return redirect('/todopage')
+        else:
+            return redirect('/loginn')
+
+    return render(request,'loginn.html')
